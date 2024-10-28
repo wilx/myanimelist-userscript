c0839df3aa9f407004d3009d0e58a41d2d0659b1
Vaclav Haisman <vhaisman@gmail.com>
2024-10-28 10:40:18 +0100

    Updates.

--- output/index.user.js
+++ output/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        myanimelist-userscript
 // @description MyAnimeList improver.
-// @version     1.0.72
+// @version     1.0.74
 // @author      wilx
 // @homepage    https://github.com/wilx/myanimelist-userscript
 // @supportURL  https://github.com/wilx/myanimelist-userscript/issues
