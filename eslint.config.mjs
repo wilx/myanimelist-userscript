d57c61f637b596554e37f25e70641640745088fc
Vaclav Haisman <vhaisman@gmail.com>
2026-03-17 19:46:22 +0100

    Updates.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,9 @@
 import globals from "globals";
-import babelParser from "@babel/eslint-parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
 
 export default [{
     ignores: ["output/*.js"],
-}, ...compat.extends("standard"), {
+}, js.configs.recommended, {
     languageOptions: {
         globals: {
             ...globals.browser,
@@ -23,25 +11,27 @@ export default [{
             Atomics: "readonly",
             SharedArrayBuffer: "readonly",
         },
-
-        parser: babelParser,
-        ecmaVersion: 11,
+        ecmaVersion: "latest",
         sourceType: "module",
-
-        parserOptions: {
-            ecmaFeatures: {
-                shippedProposals: true,
-                classStaticBlock: true,
-            },
-
-            requireConfigFile: "false",
-            babelOptions: {},
-        },
     },
 
     rules: {
-        semi: [2, "always"],
+        "array-bracket-spacing": ["error", "never"],
+        "arrow-spacing": ["error", { before: true, after: true }],
+        "comma-dangle": ["error", "never"],
+        "comma-spacing": ["error", { before: false, after: true }],
+        "dot-location": ["error", "property"],
         indent: ["error", 4],
+        "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+        "keyword-spacing": ["error", { before: true, after: true }],
+        "no-multi-spaces": "error",
+        "no-trailing-spaces": "error",
         "operator-linebreak": ["error", "before"],
+        "object-curly-spacing": ["error", "always"],
+        "space-before-blocks": ["error", "always"],
+        "space-in-parens": ["error", "never"],
+        "space-infix-ops": "error",
+        "space-unary-ops": ["error", { words: true, nonwords: false }],
+        semi: [2, "always"],
     },
 }];
